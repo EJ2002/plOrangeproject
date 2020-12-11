9ad789c5fa9e04bec195022c962b56618aec1050
EJ2002 <74745004+EJ2002@users.noreply.github.com>
2020-12-11 08:45:13 +0800

    Update StartMenu.py

--- StartMenu.py
+++ StartMenu.py
@@ -4,16 +4,16 @@ users = {}
 
 
 def login():
-    print("LOGIN")
-    user_user = input("Enter Username:")
-    user_pass: str = input("Enter Password:")
-
-    if user_user in admin and admin[user_user] == admin[user_user]:
-        print("Login Successful.")
-        import Admin
-        Admin.admin_menu()
-    else:
-        print("username or password is incorrect.")
+    print("Welcome to the Payroll System")
+    while True:
+        user_user = input("Enter Username:")
+        user_pass: str = input("Enter Password:")
+        if user_user in admin and admin[user_user] == admin[user_user]:
+            print("Login Successful.")
+            import Admin
+            Admin.admin_menu()
+        else:
+            print("username or password is incorrect.\n Please try again.")
 
 
 login()
